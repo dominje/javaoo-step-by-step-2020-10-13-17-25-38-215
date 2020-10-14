00e83aad6ef19a613733154d9cdaebbd11ab7883
dominje <jericah.domingo@oocl.com>
2020-10-14 20:26:32 +0800

    [practice07][jericah]feature: added changes

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,30 @@
 package practice07;
 
 public class Klass {
+
+    private int number;
+    private Student studentName;
+
+
+    public Student getStudentName() {
+        return studentName;
+    }
+
+    public Klass(int klass) {
+        this.number = klass;
+    }
+
+    public Klass(Student studentName) {
+        this.studentName = studentName;
+    }
+    
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
 }
