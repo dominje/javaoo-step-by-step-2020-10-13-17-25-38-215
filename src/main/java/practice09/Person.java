1ac2f47448fdd16cd2ec42ce24740e1503a6a976
dominje <jericah.domingo@oocl.com>
2020-10-18 16:36:46 +0800

    [practice09][jericah]feature: added logic that will override equals

--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -33,5 +33,16 @@ public class Person {
         return "My name is " + name + ". I am " + age + " years old.";
     }
 
+    @Override
+    public boolean equals(Object o) {
 
+        if (o == this) { return true; }
+        if (!(o instanceof Person)) { return false; }
+
+        Person c = (Person) o;
+
+        return Double.compare(id, c.id) == 0
+                && name.equals(c.name)
+                && Double.compare(age, c.age) == 0;
+    }
 }
